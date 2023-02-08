e9a4eba017e878ad797aa3a72849b0c394b44917
Pavlo Abolmasov <100512886+ilovekharkiv@users.noreply.github.com>
2023-02-08 13:37:50 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,18 +3,21 @@ def incrementJar() {
     sh 'mvn build-helper:parse-versions:set \
         -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.MinorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
         versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
 }
 def buildJar() {
     echo "building the application...please wait"
-    sh 'mvn package'
+    sh 'mvn clean package'
 }
 
 def buildDockerImage() {
     echo "building docker image"
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build -t ilovekharkiv/ilovekharkiv:myjava-1.0 .'
+    sh "docker build -t ilovekharkiv/ilovekharkiv:$IMAGE_NAME ."
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push ilovekharkiv/ilovekharkiv:myjava-1.0'
+    sh "docker push ilovekharkiv/ilovekharkiv$IMAGE_NAME"
 }
 }
 
