7b052a244ac53501c9eaed7a87e1a71f32f2ca23
Pavlo Abolmasov <abolmasovp@gmail.com>
2023-02-20 21:41:56 +0200

    added sed, just for experiment - 2

--- script.groovy
+++ script.groovy
@@ -56,9 +56,8 @@ def provisionInstance() {
 }*/
 
 def runAnsible() {
-        
+        sh "sed 's/image_name/$IMAGE_NAME/g' docker-compose.yaml"
         dir('ansible') {
-            sh "sed 's/image_name/$IMAGE_NAME/g' docker-compose.yaml"
             withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             sh "echo $PASS | ansible-playbook --inventory ${EC2_PUBLIC_IP}, --private-key /home/ubuntu/.ssh/ansible_server.pem --user ec2-user deploy-docker-ec2user.yaml --extra-vars docker_password=$PASS"
 
